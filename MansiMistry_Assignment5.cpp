9c75ba1f25ea7f3ffd39b962f97298a3358f7333
MANSI MISTRY <65220621+MansiLad@users.noreply.github.com>
2021-01-03 21:05:31 +0530

    Create MansiMistry_Assignment5.cpp
    Solution to Assignment 5

---
+++ MansiMistry_Assignment5.cpp
@@ -0,0 +1,16 @@
+//Program to sort an array in ascending order using Insertion Sort 
+void InsertionSort( int arr[], int n)
+{
+  int temp;
+  for(int i=1, i<n, i++)
+  {
+    int num = arr[i];
+    while(arr[i-1]>num && i>0)
+    {
+      temp = arr[i];
+      arr[i] = arr[i-1];
+      arr[i-1]  = temp;
+    }
+  }
+  return arr;
+}
